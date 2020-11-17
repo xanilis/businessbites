ed956777e8580ed27df7ffea2b8227b55f267630
Nils Bruder <nils.bruder@outlook.com>
2020-11-17 16:24:00 +0000

    index page skeleton

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,9 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find_by(params[:id])
   end
 end
