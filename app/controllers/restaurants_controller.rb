0e59c79534b1e2a62f3cc482527d0c434bf59ee6
Julia Kauffmann <kauffmann.julia@yahoo.de>
2020-11-20 15:40:20 +0000

    solve problem with search

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,8 @@
 class RestaurantsController < ApplicationController
   def index
     if params[:query].present?
-      @restaurants = Restaurant.search_by_name_and_food_style_and_atmosphere_and_area_and_food_philosophy_and_user_rating(params[:query])
+      @restaurants = Restaurant.full_search(params[:query])
+      @restaurants = @restaurants + Restaurant.tagged_with(params[:query])
     elsif params["search"]
       @filter = params["search"]["food_styles"].concat(params["search"]["atmospheres"]).flatten.reject(&:blank?)
       @restaurants = @filter.empty? ? Restaurant.all : Restaurant.all.tagged_with(@filter, any: true)
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews
 
-  include PgSearch::Model
-  pg_search_scope :search_by_name_and_food_style_and_atmosphere_and_area_and_food_philosophy_and_user_rating,
-    against: [ :name, :food_style, :atmosphere, :area, :food_philosophy, :user_rating ],
-    using: {
-      tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
-
   acts_as_taggable_on :food_styles
   acts_as_taggable_on :atmospheres
   acts_as_taggable_on :areas
@@ -19,4 +12,12 @@ class Restaurant < ApplicationRecord
   $areas = ["Chiado","Bairro Alto","Alfama","Cascais","Oeiras","Carcavelos","Belém","Baixa","Rossio","Alcântara","Príncipe Real","Parque das Nações"]
   $food_philosophys = ["vegan","vegetarian","seafood","traditional","modern","fusion","local","experimential","haute-cuisine"]
   $extras = ["extensive wine list","amuse-bouche","live music"]
+
+  include PgSearch::Model
+  pg_search_scope :full_search,
+    against: [ :name, :user_rating ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end
