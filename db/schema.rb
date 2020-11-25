d1b71a2034af2eba7c2f0303748d2fa1fbfbfc2c
Julia Kauffmann <kauffmann.julia@yahoo.de>
2020-11-25 16:32:09 +0000

    make book button clickable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_145709) do
+ActiveRecord::Schema.define(version: 2020_11_25_160611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_11_24_145709) do
     t.bigint "favorite_id", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "link"
     t.index ["favorite_id"], name: "index_restaurants_on_favorite_id"
   end
 
