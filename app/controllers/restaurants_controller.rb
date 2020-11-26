743d7c0e5d5b1e7480f9faaa34a262a62cd0d20a
Julia Kauffmann <kauffmann.julia@yahoo.de>
2020-11-26 16:12:20 +0000

    samll changes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,8 +12,8 @@ class RestaurantsController < ApplicationController
     elsif params[:query].present?
       @restaurants = Restaurant.full_search(params[:query])
       @restaurants = @restaurants + Restaurant.tagged_with(params[:query])
-    elsif params["search"]
 
+    elsif params["search"]
       @filter = params["search"]["food_styles"], params["search"]["atmospheres"], params["search"]["areas"], params["search"]["food_philosophys"], params["search"]["extras"], params["search"]["suitable_fors"]
       @restaurants = @filter.empty? ? Restaurant.all : Restaurant.all.tagged_with((@filter.reject &:blank?), any: true)
 
